075b2b0a121e15babdeb799ae65f7887a14f0f3c
onixldlc <81515667+onixldlc@users.noreply.github.com>
2025-03-19 17:55:18 +0700

    add: added openvpn configuration scripts

---
+++ openvpn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONFIG_PATH="${CONFIG_PATH}"
+
+if [ -z "$CONFIG_PATH" ]; then
+    echo "No config file specified! using default config path!"
+    CONFIG_PATH="/etc/openvpn/client.ovpn"
+else
+    echo "Config file specified!"
+fi
+
+if [ "$CONFIG_PATH" == "/etc/openvpn/client.ovpn" ]; then
+    echo "Using default config path!"
+else
+    echo "Using custom config path!"
+fi
+echo "config file: [$CONFIG_PATH]" 
+
+
+if [ -f "$CONFIG_PATH" ]; then
+    cp "$CONFIG_PATH" /etc/openvpn/client.ovpn
+else
+    echo "No config file found at [$CONFIG_PATH] !"
+    exit 1
+fi
+
+echo "setting up /dev/net/tun..."
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
+
+openvpn --config /etc/openvpn/client.ovpn & 
+echo "[+] openvpn started!"
